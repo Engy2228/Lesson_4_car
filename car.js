b629a62cd5c0af74671b9d34625bfb72ab0a2c84
Engy2228 <109961605+Engy2228@users.noreply.github.com>
2024-10-01 18:40:07 +0300

    Add files via upload

---
+++ car.js
@@ -0,0 +1,10 @@
+const carName = 'Kia'
+let maxCarSpeed = '220'
+    maxCarSpeed = '170'
+console.log('У машины изменилась скорость', maxCarSpeed)   
+let carOwner = 'Evgen'
+    carOwner = 'Matvey'
+console.log('У машины изменился владелец', carOwner)
+alert(carName)
+alert(maxCarSpeed)
+alert(carOwner)
